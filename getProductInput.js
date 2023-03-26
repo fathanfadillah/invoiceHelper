7f2d1603f97ce3440b2a5674c6e3a6ffe7937bd8
fathanfadillah <fathan@charged.asia>
2023-03-26 14:12:56 +0700

    getProductInput: length product by counting the row, get value input by read id which equal with index

--- getProductInput.js
+++ getProductInput.js
@@ -2,17 +2,18 @@ const getProductInput = async (getFromInput) => {
     const neWFromInput = getFromInput
     neWFromInput.product = [];
 
-    for (let index = 0; index <= productNumber; index++) {
-        const product = {};
+    const length = $('#productList .row').length
+    for (let index = 0; index < length; index++) {
+        const label = $(`#productList .row:eq(${index}) #label`).val()
+        const quantity = $(`#productList .row:eq(${index}) #quantity`).val()
+        const price = $(`#productList .row:eq(${index}) #price`).val()
+        const discount = $(`#productList .row:eq(${index}) #discount`).val()
+        const taxed = $(`#productList .row:eq(${index}) #taxed`).val()
 
-        const label = $(`#label-${index}`).val();
-        const quantity = $(`#quantity-${index}`).val();
-        const price = $(`#price-${index}`).val();
-        const discount = $(`#discount-${index}`).val();
-        const taxed = $(`#taxed-${index}`).val();
         const totalPrice = price * quantity
         const amount = totalPrice - (totalPrice * discount / 100) + (totalPrice * taxed / 100)
 
+        const product = {};
         product.label = label;
         product.quantity = quantity;
         product.price = price;
