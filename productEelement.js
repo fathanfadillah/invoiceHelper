5e97c53b2672dad4981cd0e50ced4dd64bf8df6d
fathanfadillah <fathan@charged.asia>
2023-03-26 14:10:48 +0700

    product row template: add discount symbol at discount column, tax change into dropdown

--- productEelement.js
+++ productEelement.js
@@ -30,21 +30,25 @@ const productElement = (productNumber) =>
 <div class="col">
   <input
     type="text"
-    class="form-control"
+    class="form-control w-50 me-2 d-inline"
     placeholder="Discount"
     aria-label="Discount"
     id="discount"
+    min="0"
+    max="100"
   />
+  <label for="discount" class="d-inline">%</label>
 </div>
 <div class="col">
-  <input
-    type="text"
-    class="form-control w-50 d-inline"
-    placeholder="Taxed"
-    aria-label="Taxed"
+  <select
     id="taxed"
-  />
-  <span class="ms-2 d-inline">%</span>
+    class="form-select w-75"
+    aria-label="Default select example"
+  >
+    <option selected>Tax</option>
+    <option value="11">PPN 11%</option>
+    <option value="15">PPh 15%</option>
+  </select>
 </div>
 <div class="col">
   <button id="delete" class="btn btn-danger" style="float: right" type="button" onclick="deleteRow(this)">
